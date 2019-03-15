c73530d68db2083516bf4ce97a91173954514668
puttley <puttley@pitsco.com>
2019-03-15 08:32:30 -0500

    extended blocks

--- wearable_blocks.js
+++ wearable_blocks.js
@@ -59,6 +59,7 @@ Blockly.Blocks['exp_show_image'] = {
     this.appendDummyInput()
         .appendField("show image")
         .appendField(new Blockly.FieldDropdown([["heart","150"], ["purple bird","151"], ["smiley face","152"], ["frowny face","153"], ["right arrow","154"], ["left arrow","155"], ["up arrow","156"], ["down arrow","157"]]), "images");
+        .appendField(new Blockly.FieldImage("images/blink.png", 50, 50, "*"));
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
