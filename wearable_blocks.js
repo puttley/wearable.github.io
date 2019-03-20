422472429b47984cb5f9f1eaeb32e9b5f0037e51
puttley <puttley@pitsco.com>
2019-03-20 11:57:48 -0500

    extended blocks

--- wearable_blocks.js
+++ wearable_blocks.js
@@ -119,19 +119,7 @@ Blockly.Blocks['exp_start'] = {
   }
 };
 
-Blockly.Blocks['exp_end'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("end")
-        .appendField(new Blockly.FieldImage("images/progress.gif", 15, 15, "*"))
-        .appendField("repeat")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "repeat");
-    this.setPreviousStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+
 
 Blockly.Blocks['exp_sound'] = {
   init: function() {
