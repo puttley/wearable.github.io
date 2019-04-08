21ef93b0082eebd52eefde894e320a864cacaa3f
puttley <puttley@pitsco.com>
2019-04-08 17:17:31 -0500

    expanded blocks

--- wearable_generator.js
+++ wearable_generator.js
@@ -73,7 +73,8 @@ Blockly.JavaScript['exp_start'] = function(block) {
 Blockly.JavaScript['exp_end'] = function(block) {
   var checkbox_repeat = block.getFieldValue('repeat') == 'TRUE';
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  if(checkbox_repeat == 'true') {var code = ')' + ',' + 1;}
+  if(checkbox_repeat == 'false'){var code = ')' + ',' + 0;}
   return code;
 };
 
