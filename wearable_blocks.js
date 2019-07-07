6ba77128d1b436966a9e339bf1df6ef832ec770c
puttley <puttley@pitsco.com>
2019-07-07 10:03:39 -0500

    EX

--- wearable_blocks.js
+++ wearable_blocks.js
@@ -40,7 +40,7 @@ Blockly.Blocks['exp_blink'] = {
         .appendField("blink display");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour("#ff0000");
+    this.setColour("#69F0AE");
  this.setTooltip("");
  this.setHelpUrl("");
   }
