9c6a9028c0ebffc03b39d64c4a323a0d66f54788
puttley <puttley@pitsco.com>
2019-07-30 17:59:07 -0500

    ex

--- wearable_blocks.js
+++ wearable_blocks.js
@@ -281,7 +281,7 @@ Blockly.Blocks['exp_sound'] = {
         .appendField("until done");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour("#AD1457");
+    this.setColour("#C2185B");
  this.setTooltip("");
  this.setHelpUrl("");
   }
@@ -308,7 +308,7 @@ Blockly.Blocks['exp_tone'] = {
         .appendField("seconds");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour("#AD1457");
+    this.setColour("#C2185B");
  this.setTooltip("");
  this.setHelpUrl("");
  this.setFieldValue("1", "duration");  // default check mark is 1 second
@@ -330,7 +330,7 @@ Blockly.Blocks['exp_note'] = {
         .appendField("seconds");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour("#880061");
+    this.setColour("#C2185B");
  this.setTooltip("");
  this.setHelpUrl("");
  this.setFieldValue("1", "duration");  // default check mark is 1 second
