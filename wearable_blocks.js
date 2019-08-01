2149e3eda03873eeab12cfec65e6328ad435fca0
puttley <puttley@pitsco.com>
2019-08-01 13:32:45 -0500

    ex

--- wearable_blocks.js
+++ wearable_blocks.js
@@ -31,7 +31,7 @@ Blockly.Blocks['sense_left'] = {
   init: function() {
     this.appendDummyInput()
         .appendField(new Blockly.FieldDropdown([["left","0"], ["right","1"], ["forward","2"]]), "tilt")
-        .appendField("tilt_2");
+        .appendField("tilt_3");
     this.setOutput(true, null);
     this.setColour(230);
  this.setTooltip("");
@@ -39,11 +39,11 @@ Blockly.Blocks['sense_left'] = {
 },
 onchange: function(ev) {
 
-  console.log(this.tilt.getFieldValue());
+  console.log(this.tilt.getValue());
 
-    if(this.getFieldValue.tilt == "left") {this.appendField(new Blockly.FieldImage("images/cube_left_tilt.PNG", 50, 50, "*"));}
-      if(this.getFieldValue.tilt == 'left') {this.appendField(new Blockly.FieldImage("images/cube_left_tilt.PNG", 50, 50, "*"));}
-        if(this.getFieldValue.tilt == 0) {this.appendField(new Blockly.FieldImage("images/cube_left_tilt.PNG", 50, 50, "*"));}
+//    if(this.getFieldValue.tilt == "left") {this.appendField(new Blockly.FieldImage("images/cube_left_tilt.PNG", 50, 50, "*"));}
+//      if(this.getFieldValue.tilt == 'left') {this.appendField(new Blockly.FieldImage("images/cube_left_tilt.PNG", 50, 50, "*"));}
+//        if(this.getFieldValue.tilt == 0) {this.appendField(new Blockly.FieldImage("images/cube_left_tilt.PNG", 50, 50, "*"));}
 
  }
 };
