ce93ffb6cc2235b0b22f2511a4809999a2ea978a
puttley <puttley@pitsco.com>
2019-08-01 14:36:55 -0500

    ex

--- wearable_blocks.js
+++ wearable_blocks.js
@@ -31,14 +31,14 @@ Blockly.Blocks['sense_left'] = {
   init: function() {
     this.appendDummyInput()
         var input = this.appendDummyInput()
-        .appendField('tilt');
+        .appendField('tilt 2');
         var options = [
             ['none', 'NONE'],
             [{'images': 'cube_left_tilt.PNG', 'width': 50, 'height': 50, 'alt': 'left'}, 'left'],
             [{'images': 'cube_right_tilt.PNG', 'width': 50, 'height': 50, 'alt': 'right'}, 'right']
 
         ];
-        input.appendField(new Blockly.FieldDropdown(options), 'tilt');
+        input.appendField(new Blockly.FieldDropdown("images/cube_left_tilt.PNG", 50, 50, "*"), 'tilt');
     this.setOutput(true, null);
     this.setColour(230);
     this.setTooltip("");
